ef8e37a696c8d3b0cdd578d98aa027f4c87ad0d3
samadhanabdagire571-art <samadhanabdagire571@gmail.com>
2026-07-14 15:46:09 +0530

    Add files via upload

---
+++ Arith.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a = 10;
+int b = 5;
+int sum = a + b;
+int sub = a - b;
+int mul = a * b;
+int div = a / b;
+cout<<"Sum = "<< sum;
+cout<<"Sub = "<< sub;
+cout<<"Mul = "<< mul;
+cout<<"Div = "<< div;
+return 0;
+}
+
---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int l,b;
+cout<<"Enter length and breadth"<<endl;
+cin>>l>>b;
+int Area = l * b;
+int Per = 2*l + 2*b ;
+cout<<"Area = "<<Area<<endl;
+cout<<"per = "<<Per;
+}
