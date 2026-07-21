42f55230372a0b2eb0406ea35ae6aeecce7142ee
samadhanabdagire571-art <samadhanabdagire571@gmail.com>
2026-07-21 08:50:27 +0000

    change

---
+++ Area.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    float length , width;
+
+    cout << "Enter length: ";
+    cin >> length;
+
+
+    cout << "Enter width: ";
+    cin >> width;
+
+
+    float area = length * width;
+    float perimeter = 2 * (length + width);
+
+
+    cout << "Area of Rectangle = " << area << endl;
+    cout << "Perimeter of Rectangle = " << perimeter << endl;
+
+
+    return 0;
+
+    
+}
